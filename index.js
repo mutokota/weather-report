19a9a8448e2e79f98959e4d4ee53b3c3f624b314
kota muto610 <mutoukouta0609@gmail>
2023-05-03 11:26:24 +0900

    05/03

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const button = document.querySelector(".button");
 const cityInformation = document.getElementById("cityInformation");
 
+//クリックイベントを付与
 button.addEventListener("click", () => {
   const APIkey = "c568b496fe4413cac89afabc4c798e86";
   let city = document.querySelector(".search input").value;
+  //アニメーションのクラスを削除
   cityInformation.classList.remove("information");
+  //$cityにはinputタグに入力された値と同じ都市を取得するよう設定
+  //言語は日本語に設定
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ja&appid=${APIkey}&units=metric`;
 
+  //非同期通信でリクエストを送信
   fetch(url)
+    //APIからのデータをjson形式で受け取る
     .then((response) => {
       return response.json();
     })
+    //受け取ったデータを引数dataに渡す
     .then((data) => {
+      //dataに格納されているデータ（都市名、天気、気温）を一つずつ変数に入れ定義
       const Name = data.name;
       const Weather = data.weather[0].description;
       const Metric = Math.round(data.main.feels_like) + "℃";
@@ -27,6 +35,7 @@ button.addEventListener("click", () => {
 
       console.log(data);
 
+      //switch文使用し、jsonファイル内にある天気名と画像が一致場合その天気にあった画像を出力
       const img = document.querySelector(".img img");
       switch (data.weather[0].main) {
         case "Clear":
@@ -53,8 +62,9 @@ button.addEventListener("click", () => {
           img.src = "";
           break;
       }
+      //アニメーションのクラスを付与する
       cityInformation.classList.add("information");
     });
-
+  //inputタグの中身をリセットする
   city = document.querySelector(".search input").value = "";
 });
